a8e8f1ebac3154cc4555301be6031897ded1e4fd
Matheus Felipe <mfasmatheus@gmail.com>
2024-01-20 23:24:50 -0300

    update: Create App Colors

---
+++ lib/support/style/app_colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const Color black05 = Color.fromRGBO(0, 0, 0, 0.5);
+  static const Color lightGrey = Color.fromRGBO(235, 235, 235, 1);
+  static const Color grey = Color.fromRGBO(158, 158, 158, 1);
+  static const Color darkGrey = Color.fromRGBO(128, 132, 143, 1);
+  static const Color salmon = Color.fromRGBO(224, 144, 144, 1);
+  static const Color white = Color.fromRGBO(255, 255, 255, 1);
+  static const Color white06 = Color.fromRGBO(255, 255, 255, 0.6);
+  static const Color blue = Color.fromRGBO(68, 109, 175, 1);
+  static const Color brown = Color.fromRGBO(121, 85, 72, 1);
+  static const Color transparent = Color.fromRGBO(0, 0, 0, 0);
+}
