4b5c0926fc25375c5675678bc78013deba303617
Matheus Felipe <mfasmatheus@gmail.com>
2024-01-20 23:27:46 -0300

    update: Refactor Home feature

--- lib/features/videos/videos_view.dart
+++ lib/features/videos/videos_view.dart
@@ -1,45 +1,54 @@
 import 'package:flutter/material.dart';
-import 'package:lojang_test/features/videos/video.dart';
 import 'package:lojang_test/features/videos/videos_view_model.dart';
 import 'package:lojang_test/support/components/default_list_tile.dart';
+import 'package:lojang_test/support/components/default_loading.dart';
+import 'package:lojang_test/support/style/app_colors.dart';
 
-class VideosView extends StatelessWidget {
-  VideosView({super.key});
+class VideosView extends StatefulWidget {
+  const VideosView({super.key});
 
+  @override
+  State<VideosView> createState() => _VideosViewState();
+}
+
+class _VideosViewState extends State<VideosView> {
   final viewModel = VideosViewModel();
+
+  @override
+  void initState() {
+    super.initState();
+    viewModel.getVideos();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Material(
-      borderRadius: const BorderRadius.only(topLeft: Radius.circular(32), topRight: Radius.circular(32)),
-      color: Colors.grey[50],
-      child: FutureBuilder<List<Video>>(
-        future: viewModel.getVideos(),
+      borderRadius: const BorderRadius.only(
+        topLeft: Radius.circular(32),
+        topRight: Radius.circular(32),
+      ),
+      color: AppColors.lightGrey,
+      child: ListenableBuilder(
+        listenable: viewModel,
         builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
+          if (viewModel.isLoading && viewModel.videosList.isEmpty) {
+            return const DefaultLoading();
           }
 
-          if (snapshot.hasData && snapshot.connectionState == ConnectionState.done) {
-            return Padding(
-              padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 24),
-              child: Expanded(
-                child: ListView.separated(
-                  separatorBuilder: (_, __) => const Divider(),
-                  itemCount: snapshot.data!.length,
-                  itemBuilder: (_, index) {
-                    final video = snapshot.data![index];
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 24),
+            child: Expanded(
+              child: ListView.separated(
+                separatorBuilder: (_, __) => const Divider(),
+                itemCount: viewModel.videosList.length,
+                itemBuilder: (_, index) {
+                  final video = viewModel.videosList[index];
 
-                    return DefaultListTile(
-                      video: video,
-                    );
-                  },
-                ),
+                  return DefaultListTile(video: video);
+                },
               ),
-            );
-          }
-
-          return Container();
+            ),
+          );
         },
       ),
     );
--- lib/features/videos/videos_view_model.dart
+++ lib/features/videos/videos_view_model.dart
@@ -5,7 +5,26 @@ import 'package:lojang_test/features/videos/video.dart';
 class VideosViewModel extends ChangeNotifier {
   final VideoRoutes videoRoutes = VideoRoutes();
 
-  Future<List<Video>> getVideos() async {
-    return await videoRoutes.getVideos(page: 1);
+  // ignore: prefer_final_fields
+  var _videos = <Video>[];
+  bool _isLoading = false;
+
+  List<Video> get videosList => _videos;
+  bool get isLoading => _isLoading;
+
+  void getVideos() async {
+    _changeIsLoading(isLoading: true);
+    try {
+      _videos = await videoRoutes.getVideos(page: 1);
+      _changeIsLoading(isLoading: false);
+    } catch (e) {
+      _changeIsLoading(isLoading: false);
+      throw Exception();
+    }
+  }
+
+  void _changeIsLoading({required bool isLoading}) {
+    _isLoading = isLoading;
+    notifyListeners();
   }
 }
