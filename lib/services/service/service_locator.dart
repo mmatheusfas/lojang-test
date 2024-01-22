51ce19760aa1a16146c9715deac9b0398f6a8d58
Matheus Felipe <mfasmatheus@gmail.com>
2024-01-22 19:00:28 -0300

    update: Refactor Sync Manager Helper

--- lib/services/service/service_locator.dart
+++ lib/services/service/service_locator.dart
@@ -1,10 +1,8 @@
 import 'package:get_it/get_it.dart';
 import 'package:lojang_test/api/setup/api_provider.dart';
-import 'package:lojang_test/services/local_storage/local_storage_helper.dart';
 
 final getIt = GetIt.instance;
 
 void setupServices() {
-  getIt.registerSingleton<LocalStorageDb>(LocalStorageDb());
   getIt.registerSingleton<ApiProvider>(ApiProvider());
 }
