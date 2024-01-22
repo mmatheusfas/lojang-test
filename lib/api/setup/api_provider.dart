c32f895fd11836d44f9da46e92022b50975da0b3
Matheus Felipe <mfasmatheus@gmail.com>
2024-01-22 18:57:57 -0300

    update: Delete Local Storage Helper

--- lib/api/setup/api_provider.dart
+++ lib/api/setup/api_provider.dart
@@ -2,18 +2,18 @@ import 'dart:io';
 
 import 'package:chucker_flutter/chucker_flutter.dart';
 import 'package:dio/dio.dart';
+import 'package:dio_cache_interceptor/dio_cache_interceptor.dart';
+import 'package:dio_cache_interceptor_hive_store/dio_cache_interceptor_hive_store.dart';
 import 'package:flutter/foundation.dart';
-import 'package:lojang_test/api/errors/connectivity_error.dart';
 import 'package:lojang_test/api/models/endpoint.dart';
 import 'package:lojang_test/support/constants.dart';
+import 'package:path_provider/path_provider.dart';
 
 class ApiProvider {
   final Dio _dio = Dio();
 
   ApiProvider() {
-    if (kDebugMode) {
-      _dio.interceptors.add(ChuckerDioInterceptor());
-    }
+    _initializeDio();
   }
 
   Future<Response<dynamic>> request({required Endpoint endpoint}) async {
@@ -33,8 +33,40 @@ class ApiProvider {
         options: requestOptions,
       );
       return response;
-    } on ConnectivityError catch (error) {
-      throw Exception(error.message);
+    } catch (e) {
+      throw Exception(e.toString());
+    }
+  }
+
+  void _initializeDio() async {
+    var cacheDir = await getTemporaryDirectory();
+
+    var cacheStore = HiveCacheStore(
+      cacheDir.path,
+      hiveBoxName: "lojong_test",
+    );
+
+    var customCacheOptions = CacheOptions(
+      store: cacheStore,
+      policy: CachePolicy.forceCache,
+      priority: CachePriority.high,
+      maxStale: const Duration(days: 7),
+      hitCacheOnErrorExcept: [401, 404],
+      keyBuilder: (request) {
+        return request.uri.toString();
+      },
+      allowPostMethod: false,
+    );
+
+    if (kDebugMode) {
+      _dio.interceptors.addAll(
+        [
+          ChuckerDioInterceptor(),
+          DioCacheInterceptor(options: customCacheOptions),
+        ],
+      );
+    } else {
+      _dio.interceptors.add(DioCacheInterceptor(options: customCacheOptions));
     }
   }
 }
